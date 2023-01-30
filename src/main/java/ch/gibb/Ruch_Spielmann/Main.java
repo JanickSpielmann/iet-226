639e668e08d333e3c84518627cc272225de6299b
Janick Spielmann <janick.spielmann@messageme.ch>
2023-01-30 20:12:14 +0100

    Aufgabe 1

--- src/main/java/ch/gibb/Ruch_Spielmann/Main.java
+++ src/main/java/ch/gibb/Ruch_Spielmann/Main.java
@@ -2,6 +2,26 @@ package ch.gibb.Ruch_Spielmann;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+                // Anzahl der Lernenden in einer Klasse
+                int numberOfStudents = 30;
+
+                // Zustand eines Lichtschalters
+                boolean lightSwitch = false;
+
+                // Saldo eines Bankkontos
+                double bankBalance = 1000.50;
+
+                // Bezeichnung einer bestimmten Linie auf dem Schachbrett
+                byte chessLine = 2;
+                char chessRow = 'd'; // d = Unicode
+
+    }
+    public static void xgleichy(){
+        int x=0;
+        long y= 1000;
+        // x = Y; -> Y ist nicht initialisiert
+        // x = y; -> ein long Wert kann nicht in einen kleineren Wert gespeichert werden.
+        x = (int) y; // Wenn man den wert castet (int) kann er gespeichert werden.
+
     }
 }
