99823ff8124b81316d3995d7b6f14956b071e37c
Janick Spielmann <janick.spielmann@messageme.ch>
2023-02-06 10:49:38 +0100

    Aufgabe 3.2.1 abgeschlossen

--- src/main/java/ch/gibb/Ruch_Spielmann/Main.java
+++ src/main/java/ch/gibb/Ruch_Spielmann/Main.java
@@ -21,6 +21,9 @@ public class Main {
         System.out.println("Saldo eines Bankkontos: " + bankBalance);
         System.out.println("Bezeichnung einer bestimmten Linie auf dem Schachbrett: " + chessLine);
         System.out.println("Bezeichnung einer bestimmten Reihe auf dem Schachbrett: " + chessRow);
+
+        tannenbaum(8);
+        ggt(7,2);
     }
     public static void xgleichy(){
         int x=0;
@@ -28,6 +31,32 @@ public class Main {
         // x = Y; -> Y ist nicht initialisiert
         // x = y; -> ein long Wert kann nicht in einen kleineren Wert gespeichert werden.
         x = (int) y; // Wenn man den Wert castet (int) kann er gespeichert werden.
+    }
+    public static void tannenbaum(int grosse){
+
+        String ausgabe = "";
+        for (int i = 1; i < grosse+1; i++) {
+            ausgabe += "*";
+            System.out.println(ausgabe);
+        }
+
+    }
+
+    public static void ggt(int p, int q){
+
+        int pEingabe = p;
+        int qEingabe = q;
+        if(p<q){
+            int x = q;
+            q = p;
+            p = x;
+        }
+        while(q!=0){
+            int r = p%q;
+            p = q;
+            q = r;
+        }
+        System.out.println("Der grösste gemeinsame Teiler von " + pEingabe + " und " + qEingabe + " ist " + p);
     }
 
 }
